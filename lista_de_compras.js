e98e6758c7ccd4c69ec7b1b4c04a34443c74e193
bernardo farkas <bernardovinagreiro@gmail.com>
2026-03-12 12:24:18 -0300

    array

---
+++ lista_de_compras.js
@@ -0,0 +1,14 @@
+let lista_compras = ["Arroz","Feijão","Açúcar","Salmão"];
+ lista_compras.push("Macarrão");
+  lista_compras.push("Pão");
+   lista_compras.push("Suco");
+    lista_compras.forEach(item => 
+        { console.log(item);
+
+     });
+     let cesta_frutas = ["Maçã","Manga","Uva","Limão"]; 
+     cesta_frutas.push("Pêssego");
+      cesta_frutas.push("Morango");
+        cesta_frutas.forEach(fruta => {
+         console.log(fruta); 
+        });
